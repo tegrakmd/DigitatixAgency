e2e9dab9271a679548cd84d4614cd400297a9ee7
Tegrakmd <tegrakamanda@gmail.com>
2025-01-10 02:42:19 +0200

    Update

--- assets/js/smoothscroll.js
+++ assets/js/smoothscroll.js
@@ -2,7 +2,7 @@
 // Function to set up smooth scrolling.
 const initSmoothScrolling = () => {
   // Initialize Lenis for smooth scroll effects. Lerp value controls the smoothness.
-  const lenis = new Lenis({ lerp: 0.1 });
+  const lenis = new Lenis();
 
   // Sync ScrollTrigger with Lenis' scroll updates.
   lenis.on("scroll", ScrollTrigger.update);
