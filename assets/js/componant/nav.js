5baf7c0c722ee56e120cd360fd486822fed3dded
Tegrakmd <tegrakamanda@gmail.com>
2025-02-04 18:56:37 +0200

    Update

--- assets/js/componant/nav.js
+++ assets/js/componant/nav.js
@@ -1,3 +1,60 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let tl = gsap.timeline({ paused: true });
+
+  tl.to(".menu-overlay", {
+    duration: 1.2,
+    clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+    ease: "power3.out",
+  });
+
+  tl.from(
+    ".menu-link, .btn",
+    {
+      opacity: 0,
+      y: 60,
+      stagger: 0.05,
+      duration: 0.75,
+      ease: "power1.out",
+    },
+    "<"
+  );
+
+  tl.to(
+    ".video-preview",
+    {
+      duration: 1,
+      height: "190px",
+      ease: "power2.out",
+    },
+    "<"
+  );
+
+  tl.to(
+    ".menu-divider",
+    {
+      duration: 2,
+      width: "100%",
+      ease: "power4.out",
+    },
+    "<"
+  );
+
+  function openMenu() {
+    document.querySelector(".menu-overlay").style.pointerEvents = "all";
+    tl.play();
+  }
+  function closeMenu() {
+    document.querySelector(".menu-overlay").style.pointerEvents = "none";
+    tl.reverse();
+  }
+
+  document.querySelector(".menu-btn-open").addEventListener("click", openMenu);
+  document
+    .querySelector(".menu-close-btn")
+    .addEventListener("click", closeMenu);
+  tl.reverse();
+});
+
 class MenuNav extends HTMLElement {
   constructor() {
     super();
@@ -5,7 +62,7 @@ class MenuNav extends HTMLElement {
     const basePath = this.getAttribute("base-path") || ".";
 
     this.innerHTML = `
-        <a href="#" class="menu-nav">
+        <a href="#" class="menu-nav menu-btn-open">
           <div class="menu-icon">
             <img src="${basePath}/assets/images/icons/plus.svg" alt="" />
           </div>
