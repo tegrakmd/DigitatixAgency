4d50b77c37e3ff5d4b9fa71247e5da991d63630b
Tegrakmd <tegrakamanda@gmail.com>
2025-03-04 03:59:58 +0200

    Update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,7 @@
-// import "./preloa.js";
-
-// import "./smoothscroll.js";
 import "./componant/nav.js";
 import "./componant/logo.js";
 import "./componant/footer.js";
 import "./cursor.js";
 import "./main.js";
-import "./loadimg.js";
+// import "./loadimg.js";
+import "./preloa.js";
